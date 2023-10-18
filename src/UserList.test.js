e2871f060ec5e9da0a0dc6c763023a04a17581bb
vinothsmart <s.vinothkanna90@gmail.com>
2023-10-18 15:51:54 +0530

    querySelector

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,20 +2,23 @@ import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
 test("render one row for each user", () => {
+  // render the component
   const users = [
     { id: 1, name: "jane", email: "jane@jane.com" },
     { id: 2, name: "sam", email: "sam@sam.com" },
   ];
-  // render the component
-  render(<UserList users={users} />);
+  //   render(<UserList users={users} />);
+  const { container } = render(<UserList users={users} />);
 
   // Find all the rows in the table
   //   screen.logTestingPlaygroundURL();
   //   const rows = screen.getAllByRole("row");
-  const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  //   const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  const table = container.querySelector("table");
+  console.log(table);
 
   // Assertion: correct number of rows in the table
-  expect(rows).toHaveLength(2);
+  //   expect(rows).toHaveLength(2);
 });
 
 test("render the email and name of each user", () => {});
