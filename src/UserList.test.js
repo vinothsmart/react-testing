b32a15a6b1ddd661fb742222588e17fb5e507c01
vinothsmart <s.vinothkanna90@gmail.com>
2023-10-18 18:34:33 +0530

    avoid the approach

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,14 +1,24 @@
 import { getByRole, render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row for each user", () => {
-  // render the component
+function renderComponent() {
   const users = [
     { id: 1, name: "jane", email: "jane@jane.com" },
     { id: 2, name: "sam", email: "sam@sam.com" },
   ];
   render(<UserList users={users} />);
 
+  return { users };
+}
+
+// avoid the approach below above because it's not a good practice above is better
+// beforeEach(() => {
+//   render(<UserList users={users} />);
+// });
+
+test("render one row for each user", () => {
+  // render the component
+  renderComponent();
   // Find all the rows in the table
   const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
@@ -18,11 +28,7 @@ test("render one row for each user", () => {
 
 test("render the email and name of each user", () => {
   // render the component
-  const users = [
-    { id: 1, name: "jane", email: "jane@jane.com" },
-    { id: 2, name: "sam", email: "sam@sam.com" },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   //   screen.logTestingPlaygroundURL();
   for (let user of users) {
